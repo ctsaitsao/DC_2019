6853dc477e329acd39b348232d04f5a016027891
juanmariasolera <49822840+juanmariasolera@users.noreply.github.com>
2019-05-06 22:56:51 -0500

    Update MAIN.ino

--- MAIN.ino
+++ MAIN.ino
@@ -1,20 +1,28 @@
 #include <Servo.h>
-#define FWD 1
-#define BACK 0
+#define FWD 0
+#define BACK 1
 
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
-int pos = 0;    // variable to store the servo position
-int left_wheel_PWM = 9;    // LED connected to digital pin 9
-int left_wheel_Dir = 10;
-int right_wheel_PWM = 11;
-int right_wheel_Dir = 12;
+int pos = 45;    // variable to store the servo position
+const int left_wheel_PWM = 3;   
+const int left_wheel_Dir = 10;
+const int right_wheel_PWM = 11;
+const int right_wheel_Dir = 12;
 
 
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(13);  // attaches the servo on pin 13 to the servo object
   myservo.write(0);
+
+  pinMode(left_wheel_PWM, OUTPUT);
+  pinMode(right_wheel_PWM, OUTPUT);
+
+  analogWrite(left_wheel_PWM, 0);
+  digitalWrite(left_wheel_Dir, FWD);
+  analogWrite(right_wheel_PWM, 0);
+  digitalWrite(right_wheel_Dir, FWD);  
 
 }
 
@@ -35,8 +43,8 @@ void loop() {
       analogWrite(left_wheel_PWM, 120);
       digitalWrite(left_wheel_Dir, FWD);
 
-      analogWrite(left_wheel_PWM, 120);
-      digitalWrite(left_wheel_Dir, FWD);   
+      analogWrite(right_wheel_PWM, 120);
+      digitalWrite(right_wheel_Dir, FWD);   
   }
 
      // 's': both wheels move backward, robot moves backward
@@ -47,8 +55,8 @@ void loop() {
       analogWrite(left_wheel_PWM, 120);
       digitalWrite(left_wheel_Dir, BACK);
 
-      analogWrite(left_wheel_PWM, 120);
-      digitalWrite(left_wheel_Dir, BACK);   
+      analogWrite(right_wheel_PWM, 120);
+      digitalWrite(right_wheel_Dir, BACK);   
   }
 
      // 'a': left wheel moves backward, right wheel moves forward, robot rotates left
@@ -58,8 +66,8 @@ void loop() {
       analogWrite(left_wheel_PWM, 120);
       digitalWrite(left_wheel_Dir, FWD);
 
-      analogWrite(left_wheel_PWM, 120);
-      digitalWrite(left_wheel_Dir, BACK);   
+      analogWrite(right_wheel_PWM, 120);
+      digitalWrite(right_wheel_Dir, BACK);   
   
   }
 
@@ -70,35 +78,42 @@ void loop() {
       analogWrite(left_wheel_PWM, 120);
       digitalWrite(left_wheel_Dir, BACK);
 
-      analogWrite(left_wheel_PWM, 120);
-      digitalWrite(left_wheel_Dir, FWD);   
+      analogWrite(right_wheel_PWM, 120);
+      digitalWrite(right_wheel_Dir, FWD);   
   }
 
-  if (Key == 'f') {
+  if (Key == 'f') {                 // set different speeds (0-100%)
+    if (Serial.available()) {
     int PWM_freq = Serial.read();
     PWM_freq = PWM_freq * 255 / 100;
     analogWrite(left_wheel_PWM, PWM_freq);
     analogWrite(right_wheel_PWM, PWM_freq);
+    }
   }
 
+  
+  if (Key == 'h') {
+    digitalWrite(left_wheel_PWM, 0);
+    digitalWrite(right_wheel_PWM, 0);
+  }
   
   //cases for the arm
 
   // 'k' lowers the arm into "attack" so it can grasp onto the obstacles
   if (Key == 'k') {
-  for (pos = 0; pos <= 120; pos += 1) { // goes from 0 degrees to 180 degrees
+  //for (pos = 45; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+    myservo.write(180);              // tell servo to go to position in variable 'pos'
+  //  delay(15);                       // waits 15ms for the servo to reach the position
+ // }
   }
 
   // 'i' raises the arm into retreat
   if (Key == 'i') {
-  for (pos = 120; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
-  }
+  //for (pos = 180; pos >= 45; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(45);              // tell servo to go to position in variable 'pos'
+   // delay(15);                       // waits 15ms for the servo to reach the position
+ // }
 }
 
 
